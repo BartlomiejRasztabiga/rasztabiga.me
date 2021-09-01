0ebbfced788691b02a85319f6e1edfc3903921e9
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2021-09-01 19:55:36 +0200

    update about me page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { graphql } from 'gatsby';
+import {graphql} from 'gatsby';
 import get from 'lodash/get';
 import React from 'react';
 
@@ -11,27 +11,27 @@ import SectionProjects from '../components/section-projects';
 import SectionSkills from '../components/section-skills';
 import SEO from '../components/seo';
 
-const Index = ({ data }) => {
-  const about = get(data, 'site.siteMetadata.about', false);
-  const projects = get(data, 'site.siteMetadata.projects', false);
-  const posts = data.allMarkdownRemark.edges;
-  const experience = get(data, 'site.siteMetadata.experience', false);
-  const skills = get(data, 'site.siteMetadata.skills', false);
-  const noBlog = !posts || !posts.length;
+const Index = ({data}) => {
+    const about = get(data, 'site.siteMetadata.about', false);
+    const projects = get(data, 'site.siteMetadata.projects', false);
+    const posts = data.allMarkdownRemark.edges;
+    const experience = get(data, 'site.siteMetadata.experience', false);
+    const skills = get(data, 'site.siteMetadata.skills', false);
+    const noBlog = !posts || !posts.length;
 
-  return (
-    <Layout>
-      <SEO />
-      <Header metadata={data.site.siteMetadata} noBlog={noBlog} />
-      {about && <SectionAbout about={about} />}
-      {projects && projects.length && <SectionProjects projects={projects} />}
-      {!noBlog && <SectionBlog posts={posts} />}
-      {experience && experience.length && (
-        <SectionExperience experience={experience} />
-      )}
-      {skills && skills.length && <SectionSkills skills={skills} />}
-    </Layout>
-  );
+    return (
+        <Layout>
+            <SEO/>
+            <Header metadata={data.site.siteMetadata} noBlog={noBlog}/>
+            {about && <SectionAbout about={about}/>}
+            {experience && experience.length && (
+                <SectionExperience experience={experience}/>
+            )}
+            {skills && skills.length && <SectionSkills skills={skills}/>}
+            {projects && projects.length && <SectionProjects projects={projects}/>}
+            {!noBlog && <SectionBlog posts={posts}/>}
+        </Layout>
+    );
 };
 
 export default Index;
